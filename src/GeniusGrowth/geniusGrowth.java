87c53a8a87f91e3f9e9baf5cd5d29b01fd857a37
Afnan Farhat <4fnan.f@gmail.com>
2023-11-10 19:08:06 +0300

    change class name!

--- src/GeniusGrowth/geniusGrowth.java
+++ src/GeniusGrowth/geniusGrowth.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package s;
+package GeniusGrowth;
 
 /**
  *
  * @author Afnan
  */
-public class S {
+public class geniusGrowth {
 
     /**
      * @param args the command line arguments
